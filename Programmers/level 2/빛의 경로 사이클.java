8b4b7ab654b39b1cd6390f6900baeb6614fc3fdb
dayun <50140949+nuyadkrap@users.noreply.github.com>
2021-10-05 00:53:54 +0900

    Update 빛의 경로 사이클.java

--- Programmers/level 2/빛의 경로 사이클.java
+++ Programmers/level 2/빛의 경로 사이클.java
@@ -1,8 +1,43 @@
-/* 
-각 칸마다 S, L, 또는 R가 써져 있는 격자가 있습니다. 당신은 이 격자에서 빛을 쏘고자 합니다. 이 격자의 각 칸에는 다음과 같은 특이한 성질이 있습니다.
-
-빛이 "S"가 써진 칸에 도달한 경우, 직진합니다.
-빛이 "L"이 써진 칸에 도달한 경우, 좌회전을 합니다.
-빛이 "R"이 써진 칸에 도달한 경우, 우회전을 합니다.
-빛이 격자의 끝을 넘어갈 경우, 반대쪽 끝으로 다시 돌아옵니다. 예를 들어, 빛이 1행에서 행이 줄어드는 방향으로 이동할 경우, 같은 열의 반대쪽 끝 행으로 다시 돌아옵니다.
-당신은 이 격자 내에서 빛이 이동할 수 있는 경로 사이클이 몇 개 있고, 각 사이클의 길이가 얼마인지 알고 싶습니다. 경로 사이클이란, 빛이 이동하는 순환 경로를 의미합니다. */
+import java.util.*;
+class Solution {
+    static int R, C;
+    static int[] dx = { -1, 0, 1, 0 }, dy = { 0, -1, 0, 1 };
+    static boolean[][][] visited;
+    public int[] solution(String[] grid) {
+        List<Integer> ans = new ArrayList<Integer>();
+        R = grid.length;
+        C = grid[0].length();
+        visited = new boolean[R][C][4];
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                for(int d=0;d<4;d++){
+                    if(!visited[i][j][d]){
+                        ans.add(go(grid,i,j,d));
+                    }
+                }
+            }
+        }
+        int[] answer = new int[ans.size()];
+        for(int i=0;i<ans.size();i++){
+            answer[i] = ans.get(i);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+    public static int go(String arr[],int x, int y, int d){
+        int cnt =0;
+        while(true){
+            if(visited[x][y][d])break;
+            cnt++;
+            visited[x][y][d] = true;
+            if(arr[x].charAt(y)=='L')
+                d = (d+3)%4;
+            else if(arr[x].charAt(y)=='R')
+                d = (d+1)%4;
+            
+            x = (x+R+dx[d])%R;
+            y = (y+C+dy[d])%C;
+        }
+        return cnt;
+    }
+}
