401cfaf554ab97290b7ee79c651b8d918ca02cb1
dayun <50140949+nuyadkrap@users.noreply.github.com>
2021-10-27 18:54:35 +0900

    [211027] 조이스틱

--- Programmers/level 2/조이스틱.java
+++ Programmers/level 2/조이스틱.java
@@ -1,1 +1,21 @@
-/* 조이스틱 */
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int len = name.length();
+        int min = len-1;
+        for(int i=0;i<len;i++){
+            if(name.charAt(i)<='M'){
+                answer += name.charAt(i)-'A';
+            }else{
+                answer += 'Z'-name.charAt(i)+1;
+            }
+            int next = i+1;
+            while(next<len && name.charAt(next)=='A'){
+                ++next;
+            }
+            min = Math.min(min,i+len-next+Math.min(i,len-next));
+        }
+        answer += min;
+        return answer;
+    }
+}
