9bc7b324d83913d7c7727bc7b93b8008ddd94b0d
dayun <50140949+nuyadkrap@users.noreply.github.com>
2021-11-17 23:50:26 +0900

    [211117] 구명보트

---
+++ Programmers/level 2/구명보트.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int i=0;
+        int j;
+        Arrays.sort(people);
+        for(j=people.length-1;i<=j;j--){
+            if(people[j]+people[i]>limit) answer++;
+            else {
+                answer++;
+                i++;
+            }
+        }
+        return answer;
+    }
+}
