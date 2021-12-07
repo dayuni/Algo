da21f170bead48ecf7514c22d3a533e3e6a76fc8
dayun <50140949+nuyadkrap@users.noreply.github.com>
2021-12-07 23:08:36 +0900

    [211207] 올바른 괄호

---
+++ Programmers/level 2/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        char arr[] = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]=='('){
+                stack.push(arr[i]);
+            }else{
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(!stack.isEmpty()) return false;
+        
+        return answer;
+    }
+}
